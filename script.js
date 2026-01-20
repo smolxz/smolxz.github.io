200de433948512e0eaa5b15188b892142a1fc97f
smolxz <smolgamexz97@outlook.com>
2026-01-20 14:16:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ form.addEventListener("submit", async function (e) {
     button.disabled = false;
   }
 });
+
+// ADD THIS TO THE END OF script.js
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuToggle) {
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
+
